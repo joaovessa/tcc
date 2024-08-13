56204590f3799a687a55aab39e4653d1ce136411
João Vitor Silva <jvsa.110507@gmail.com>
2024-08-13 16:17:50 -0300

    esperar o marcio

--- src/App.jsx
+++ src/App.jsx
@@ -3,33 +3,15 @@ import bg from './assets/bg/home_bg.png'
 import Logo from './assets/bg/icone.jpg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function Example() {
+  const [opened, setOpened] = useState(false);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={Logo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={bg} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    
+    <div class="tham tham-e-squeeze tham-w-6">
+    <div class="tham-box">
+      <div class="tham-inner" />
+    </div>
+  </div>
+  );
 }
-
-export default App
