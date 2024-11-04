65d6dbb3ab21ff96a41d89763af2e696b0face13
João Vitor Silva <jvsa.110507@gmail.com>
2024-11-04 19:52:18 +0000

    alteração background

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,11 +1,11 @@
-
+import malaBg from "../assets/mala/background.jpg"
 
 export const theme = {
     barra: "#6500F5",
     white: "#FFFFFF",
     textColor: "#FFFDFD",
     Homebackground: "#250075",
-    backgroundImage: "url('/src/assets/mala/background.jpg')",
+    backgroundImage: `url(${malaBg})`,
     fundo: "url('/src/assets/mala/fundo-desktop.jpg')"
     
 } 
